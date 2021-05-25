b1437f9bc8c820d4752549761103b6feb1242091
Minh Lượng <66545208+MinhLuong-077@users.noreply.github.com>
2021-05-25 11:04:55 +0700

    Add Docstring

--- Tranning/Test_Sentences.py
+++ Tranning/Test_Sentences.py
@@ -1,19 +1,21 @@
 import keras
 
-from load_data import load
-from build_model import build
+from Load_Data import Processing_data as load
+from Build_Model import build
 import pandas as pd
 import random as r
 import os
+
 
 class Test(build.Model_RoBERTa):
     '''
         Class test project.
     '''
-    def __init__(self,MAX_LEN,PATH,path):
-        super().__init__(MAX_LEN,PATH)
-        self.path=path
-        self.model=super().build_model()
+
+    def __init__(self, MAX_LEN, PATH, path):
+        super().__init__(MAX_LEN, PATH)
+        self.path = path
+        self.model = super().build_model()
         self.VER = 'v0'
         self.DISPLAY = 1
         self.skf = load.library.Stra_Kfold(5, True, 777)
@@ -22,8 +24,9 @@ class Test(build.Model_RoBERTa):
             1: 'positive',
             2: 'negative'
         }
-#Test model
-    def Test_Model(self,test):
+# Test model
+
+    def Test_Model(self, test):
         '''
             Đưa câu test vào model.
         :param test: câu test
@@ -32,27 +35,32 @@ class Test(build.Model_RoBERTa):
         # INPUT_IDS
         ct = test.shape[0]
         input_ids_t = load.library.np.ones((ct, self.MAX_LEN), dtype='int32')
-        attention_mask_t = load.library.np.zeros((ct, self.MAX_LEN), dtype='int32')
-        token_type_ids_t = load.library.np.zeros((ct, self.MAX_LEN), dtype='int32')
+        attention_mask_t = load.library.np.zeros(
+            (ct, self.MAX_LEN), dtype='int32')
+        token_type_ids_t = load.library.np.zeros(
+            (ct, self.MAX_LEN), dtype='int32')
 
         for k in range(test.shape[0]):
             # INPUT_IDS
             text1 = " " + " ".join(test.loc[k, 'text'].split())
             enc = load.tokenizer.encode(text1)
             s_tok = load.sentiment_id[test.loc[k, 'sentiment']]
-            input_ids_t[k, :len(enc.ids) + 5] = [0] + enc.ids + [2, 2] + [s_tok] + [2]
+            input_ids_t[k, :len(enc.ids) + 5] = [0] + \
+                enc.ids + [2, 2] + [s_tok] + [2]
             attention_mask_t[k, :len(enc.ids) + 5] = 1
         self.model.load_weights(self.path)
-        preds_start = load.library.np.zeros((input_ids_t.shape[0], self.MAX_LEN))
+        preds_start = load.library.np.zeros(
+            (input_ids_t.shape[0], self.MAX_LEN))
         preds_end = load.library.np.zeros((input_ids_t.shape[0], self.MAX_LEN))
         print('Predicting Test...')
-        preds = self.model.predict([input_ids_t, attention_mask_t, token_type_ids_t], verbose=self.DISPLAY)
+        preds = self.model.predict(
+            [input_ids_t, attention_mask_t, token_type_ids_t], verbose=self.DISPLAY)
         preds_start += preds[0]/self.skf.n_splits
         preds_end += preds[1]/self.skf.n_splits
         all = []
         for k in range(input_ids_t.shape[0]):
-            a = load.library.np.argmax(preds_start[k,])
-            b = load.library.np.argmax(preds_end[k,])
+            a = load.library.np.argmax(preds_start[k, ])
+            b = load.library.np.argmax(preds_end[k, ])
             if a > b:
                 st = test.loc[k, 'text']
             else:
@@ -61,7 +69,8 @@ class Test(build.Model_RoBERTa):
                 st = load.tokenizer.decode(enc.ids[a - 1:b])
             all.append(st)
         return all
-#Load dữ liệu người dùng nhập
+# Load dữ liệu người dùng nhập
+
     def Text(self):
         '''
             Xử lí câu input.
@@ -79,7 +88,8 @@ class Test(build.Model_RoBERTa):
                     break
             while True:
                 try:
-                    n = int(input("[0:'neutral',1:'positive',2:'negative']=  "))
+                    n = int(
+                        input("[0:'neutral',1:'positive',2:'negative']=  "))
                     if n > 2 or n < 0:
                         raise ValueError
                 except:
@@ -95,7 +105,8 @@ class Test(build.Model_RoBERTa):
             df = df.append(data, ignore_index=True)
             while True:
                 try:
-                    k = int(input("Nếu bạn ko thêm text nữa thì nhập 0, nhập số bất kỳ để thêm text "))
+                    k = int(
+                        input("Nếu bạn ko thêm text nữa thì nhập 0, nhập số bất kỳ để thêm text "))
                 except:
                     print('Phải nhập số')
                 else:
@@ -104,7 +115,8 @@ class Test(build.Model_RoBERTa):
                 break
         return df
         # df[["textID", "text", "sentiment", "selected_text"]].to_csv('D:/UIT LEARN/Năm 3 Kì 2/Python/do_an/doAN/Dataset/submission.csv', index=False)
-#Đưa file CSV để load
+# Đưa file CSV để load
+
     @staticmethod
     def Text_CSV():
         '''
@@ -130,25 +142,30 @@ class Test(build.Model_RoBERTa):
                         print('Bộ dữ liệu không hợp lệ')
                     else:
                         break
-        return test,k
-#Test 1 câu không cần lưu chỉ test để xem kết quả
-    def Text_Speed_Sentences(self,str1,sentiment):
+        return test, k
+# Test 1 câu không cần lưu chỉ test để xem kết quả
+
+    def Text_Speed_Sentences(self, str1, sentiment):
         '''
             Test 1 câu nhưng không lưu kết quả.
         :param str1: Câu test.
         :param sentiment: Sentiment của câu test.
         '''
         input_ids_t = load.library.np.ones((1, self.MAX_LEN), dtype='int32')
-        attention_mask_t = load.library.np.zeros((1, self.MAX_LEN), dtype='int32')
-        token_type_ids_t = load.library.np.zeros((1, self.MAX_LEN), dtype='int32')
-        preds_start = load.library.np.zeros((input_ids_t.shape[0], self.MAX_LEN))
+        attention_mask_t = load.library.np.zeros(
+            (1, self.MAX_LEN), dtype='int32')
+        token_type_ids_t = load.library.np.zeros(
+            (1, self.MAX_LEN), dtype='int32')
+        preds_start = load.library.np.zeros(
+            (input_ids_t.shape[0], self.MAX_LEN))
         preds_end = load.library.np.zeros((input_ids_t.shape[0], self.MAX_LEN))
         text1 = " " + " ".join(str1.split())
         enc = load.tokenizer.encode(text1)
         s_tok = load.sentiment_id[sentiment]
-        input_ids_t[0, :len(enc.ids) + 5] = [0] + enc.ids + [2, 2] + [s_tok] + [2]
+        input_ids_t[0, :len(enc.ids) + 5] = [0] + \
+            enc.ids + [2, 2] + [s_tok] + [2]
         attention_mask_t[0, :len(enc.ids) + 5] = 1
-        #Nếu load 5 fold với 5 model đã train thì kết quả tốt hơn
+        # Nếu load 5 fold với 5 model đã train thì kết quả tốt hơn
         # for fold in range(0,model_ct):
         #     self.model.load_weights('/content/drive/MyDrive/NLP/backup/%s-roberta-%i.h5'%(self.VER,fold))
         #     print('Predicting Test...')
@@ -157,7 +174,8 @@ class Test(build.Model_RoBERTa):
         #     preds_start += preds[0] / self.skf.n_splits
         #     preds_end += preds[1] / self.skf.n_splits
         self.model.load_weights(self.path)
-        preds = self.model.predict([input_ids_t, attention_mask_t, token_type_ids_t],verbose=self.DISPLAY)
+        preds = self.model.predict(
+            [input_ids_t, attention_mask_t, token_type_ids_t], verbose=self.DISPLAY)
         preds_start += preds[0] / self.skf.n_splits
         preds_end += preds[1] / self.skf.n_splits
         a = load.library.np.argmax(preds_start)
@@ -168,34 +186,41 @@ class Test(build.Model_RoBERTa):
             text1 = " " + " ".join(str1.split())
             enc = load.tokenizer.encode(text1)
             st = load.tokenizer.decode(enc.ids[a - 1:b])
-        print("text: {}   sentiment: {}   selected_text:  {}".format(str1,sentiment,st))
-#Kết quả lưu vào file csv minh tạo để lưu khi người dùng nhập
+        print("text: {}   sentiment: {}   selected_text:  {}".format(
+            str1, sentiment, st))
+# Kết quả lưu vào file csv minh tạo để lưu khi người dùng nhập
+
     @staticmethod
-    def Result(test,all):
+    def Result(test, all):
         '''
             Kết quả lưu vào file csv minh tạo để lưu khi người dùng nhập
         '''
         test['selected_text'] = all
         if not os.path.isfile('/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/submission.csv'):
-            test.to_csv('/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/submission.csv', index=False)
+            test.to_csv(
+                '/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/submission.csv', index=False)
         else:
-            with open('/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/submission.csv', 'a', encoding="utf-8",newline='') as f:
-                test.to_csv(f, index=False, header=f.tell()==0)
-        print(test[['text','selected_text']])
-#Kết quả lưu vào file CSV khi đưa vào để test,sample_submission.csv lưu toàn bộ dữ liệu được test từ file csv (nhiều file csv)
+            with open('/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/submission.csv', 'a', encoding="utf-8", newline='') as f:
+                test.to_csv(f, index=False, header=f.tell() == 0)
+        print(test[['text', 'selected_text']])
+# Kết quả lưu vào file CSV khi đưa vào để test,sample_submission.csv lưu toàn bộ dữ liệu được test từ file csv (nhiều file csv)
+
     @staticmethod
-    def Result_CSV(test,all,link_file=None):
+    def Result_CSV(test, all, link_file=None):
         '''
         Kết quả lưu vào file CSV khi đưa vào để test.
         Sample_submission.csv lưu toàn bộ dữ liệu được test từ một/nhiều file csv
         '''
         test['selected_text'] = all
         if not os.path.isfile('/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/sample_submission.csv'):
-            test.to_csv('/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/sample_submission.csv', index=False)
+            test.to_csv(
+                '/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/sample_submission.csv', index=False)
         else:
-            with open('/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/sample_submission.csv', 'a', encoding="utf-8",newline='') as f:
-                test.to_csv(f, index=False, header=f.tell()==0)
+            with open('/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/sample_submission.csv', 'a', encoding="utf-8", newline='') as f:
+                test.to_csv(f, index=False, header=f.tell() == 0)
         test.to_csv(link_file, index=False)
         print(test['selected_text'])
-path='/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/Code_Luong/Backup/v0-roberta-4.h5'
+
+
+path = '/content/drive/MyDrive/Kỹ thuật lập trình Python/Code_Python/Code_Luong/Backup/v0-roberta-4.h5'
 a = Test(load.MAX_LEN, load.PATH, path)
